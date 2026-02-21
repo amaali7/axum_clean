22aae95801c098552c04ba858ecad3ae58d0ac20
Abdallah Ali <amaali1991@gmail.com>
2026-02-21 03:59:00 +0200

    start interface layer

--- packages/infrastructure/src/lib.rs
+++ packages/infrastructure/src/lib.rs
@@ -1,9 +1,6 @@
-pub mod config;
 pub mod database;
 pub mod error;
-pub mod http;
-pub mod mappers;
 pub mod serialization;
-pub mod utils;
 
 pub static DATETIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S %:z";
+interface
